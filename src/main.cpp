0710fcb822951b29a7037cdb1cea4a7e52ca279a
s-anagpal <s-anagpal@lwsd.org>
2023-02-28 08:59:08 -0800

    move network details to a new header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <Arduino.h>
 #include <ESP8266WebServer.h>
 #include "ExcelEESD.h"
+#include "network_details.h"
 
 ESP8266WebServer server(80);
 
-// Replace with your network credentials
-const char *ssid = "NETWORK-NAME";
-const char *pass = "NETWORK-PASS";
 const String deviceId = "data-test-2";
 
 const String columns[] = {
